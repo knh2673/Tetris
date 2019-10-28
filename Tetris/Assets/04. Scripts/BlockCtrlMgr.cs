0f6a8769d5012976efc8b59de4b0116508241119
knh2673 <53848061+knh2673@users.noreply.github.com>
2019-10-28 13:52:08 +0900

    191028
    Input Keyboard

---
+++ Tetris/Assets/04. Scripts/BlockCtrlMgr.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockCtrlMgr : MonoBehaviour
+{
+    [SerializeField] private WaitForSeconds waitSec;
+
+    private GameObject block;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waitSec = new WaitForSeconds(2.0f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputKey();
+    }
+
+    private void InputKey()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Debug.Log("left");
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Debug.Log("right");
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up");
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down");
+        }
+    }
+
+    IEnumerator PullDownBlock()
+    {
+        yield return waitSec;
+    }
+}
