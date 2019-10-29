b18d531c42ac6755243b8587ad61515c9bbb2e76
knh2673 <53848061+knh2673@users.noreply.github.com>
2019-10-29 12:50:53 +0900

    191029

--- Tetris/Assets/04. Scripts/BlockCtrlMgr.cs
+++ Tetris/Assets/04. Scripts/BlockCtrlMgr.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class BlockCtrlMgr : MonoBehaviour
 {
+    [SerializeField] private GameObject[] prefebs = new GameObject[7];
+
     [SerializeField] private WaitForSeconds waitSec;
 
     private GameObject block;
+    private bool[,] tetrisPanel;
 
     // Start is called before the first frame update
     void Start()
     {
-        waitSec = new WaitForSeconds(2.0f);
+        Initialize();
 
+        CreateBlock();
+        
     }
 
     // Update is called once per frame
@@ -38,6 +43,29 @@ public class BlockCtrlMgr : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             Debug.Log("Down");
+        }
+    }
+
+    private void Initialize()
+    {
+        waitSec = new WaitForSeconds(2.0f);
+
+        tetrisPanel = TetrisMgr.Instance.TetrisPanel;
+
+        tetrisPanel[0, 0] = true;
+    }
+
+    private void CreateBlock()
+    {
+        block = Instantiate<GameObject>(prefebs[Random.Range(0,7)]);
+
+        block.transform.position = new Vector3(-0.01f, 19.0f, 5.0f);
+        
+        for(int i=0; i < block.transform.childCount; ++i)
+        {
+            Transform _tr = block.transform.GetChild(i);
+
+            tetrisPanel[(int)_tr.position.z, (int)_tr.position.y] = true;
         }
     }
 
--- Tetris/Assets/04. Scripts/TetrisMgr.cs
+++ Tetris/Assets/04. Scripts/TetrisMgr.cs
@@ -7,7 +7,35 @@ using UnityEngine;
 
 public class TetrisMgr : MonoBehaviour
 {
+    private static TetrisMgr instance = null;
+    public static TetrisMgr Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
 
+    private bool[,] tetrisPanel = new bool[10, 22];
+    public bool[,] TetrisPanel
+    {
+        get
+        {
+            return tetrisPanel;
+        }
+    }
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +44,8 @@ public class TetrisMgr : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+
 }
